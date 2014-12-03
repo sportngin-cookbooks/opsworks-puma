039b541269d6eefb87731a5f499966008fc87018
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-12-03 10:10:45 -0600

    Define puma app service and start it at the end of chef run.

--- definitions/puma_web_app.rb
+++ definitions/puma_web_app.rb
@@ -2,6 +2,12 @@ define :puma_web_app do
   deploy = params[:deploy]
   application = params[:application]
 
+  service application do
+    supports :start => true, :stop => true, :restart => true
+    action :nothing
+    notifies :reload, "service[#{application}]", :delayed
+  end
+
   nginx_web_app deploy[:application] do
     docroot deploy[:absolute_document_root]
     server_name deploy[:domains].first
